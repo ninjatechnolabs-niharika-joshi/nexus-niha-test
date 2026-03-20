fa64103585294dbbe9423741896b6c7ed08abb26
niharika-ninja <niharika@ninjatechnolabs.com>
2026-03-20 14:07:11 +0530

    changes for routing

---
+++ generate-htaccess.js
@@ -0,0 +1,18 @@
+import { writeFileSync } from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const htaccessContent = `<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+  RewriteRule ^index\\.html$ - [L]
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteRule . /index.html [L]
+</IfModule>
+`
+
+writeFileSync(join(__dirname, 'dist', '.htaccess'), htaccessContent)
+console.log('.htaccess file has been created in dist/')
